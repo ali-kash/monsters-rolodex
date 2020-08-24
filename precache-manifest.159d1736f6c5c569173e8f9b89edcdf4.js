e73ef944ba29693bfa03e0eda227b03ef0c787c4
AKash <ali.rf.kashani@gmail.com>
2020-08-24 13:17:40 -0700

    Updates

---
+++ precache-manifest.159d1736f6c5c569173e8f9b89edcdf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c68693e1ff044d089757d7cb20cc54",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "023bd574d7efe9f53b56",
+    "url": "/monsters-rolodex/static/css/main.a4174c36.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "023bd574d7efe9f53b56",
+    "url": "/monsters-rolodex/static/js/main.292c9c76.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
